a250ed16dc43332dade1676a9eb6e58a40be82a7
Byron Sommardahl <byron@acklenavenue.com>
2018-09-15 09:42:18 -0500

    Making loggly optional based on config.

--- src/app.logger.ts
+++ src/app.logger.ts
@@ -4,17 +4,23 @@ import { Loggly } from 'winston-loggly-bulk';
 import * as dotenv from 'dotenv';
 dotenv.config();
 
+const logglySubdomain = process.env.LOGGLY_SUBDOMAIN;
+const logglyToken = process.env.LOGGLY_TOKEN;
+
+const getLogglyTransport = () => {
+    return new Loggly({
+        subdomain: logglySubdomain,
+        token: logglyToken,
+        tags: ["backend"],
+        level: process.env.LOGGLY_LEVEL || "info",
+        json: true
+    });
+};
+
 export class AppLogger implements LoggerService {
     private logger;
 
     constructor() {
-        const loggly = new Loggly({
-            subdomain: process.env.LOGGLY_SUBDOMAIN,
-            token: process.env.LOGGLY_TOKEN,
-            tags: ["backend"],
-            level: process.env.LOGGLY_LEVEL || "info",
-            json: true
-        });
 
         const logConsole = new winston.transports.Console({
             level: 'debug',
@@ -22,11 +28,17 @@ export class AppLogger implements LoggerService {
             format: winston.format.simple(),
         });
 
+        const transports = [
+            logConsole,
+        ];
+
+        const hasLogglyConfig = logglySubdomain && logglyToken;
+        if (hasLogglyConfig) {
+            transports.push(getLogglyTransport());
+        }
+
         this.logger = winston.createLogger({
-            transports: [
-                logConsole,
-                loggly
-            ]
+            transports
         });
     }
 
