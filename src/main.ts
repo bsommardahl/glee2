cd006a09990cd06ccbfc8066448267aed69c5292
cefeboru <cefeboru@gmail.com>
2018-09-15 11:36:06 -0600

    Adding prefix and bearer auth to swagger docs

--- src/main.ts
+++ src/main.ts
@@ -5,19 +5,23 @@ import { JwtAuthGuard } from './modules/auth/jwt-auth.guard';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix(process.env.API_VERSION || 'v1');
-  app.useGlobalGuards(new JwtAuthGuard());
+  const apiVersionPrefix:string = process.env.API_VERSION || 'v1';
+  app.setGlobalPrefix(apiVersionPrefix);
   const options = new DocumentBuilder()
     .setTitle('Glee2 Boilerplate')
     .setDescription('The API description')
     .setVersion('1.0')
     .addTag('customTag')
+    .setBasePath(apiVersionPrefix)
+    .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('api', app, document);
+  
+  SwaggerModule.setup(`api/${apiVersionPrefix}`, app, document);
+
   const port = parseInt(process.env.SERVER_PORT || "3000", 10);
   await app.listen(port);
   console.log(`Listening on port ${port}.`);
-
 }
+
 bootstrap();
