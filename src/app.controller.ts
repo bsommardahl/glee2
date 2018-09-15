a3b823df66b9223cae4c19a32039c47f61073b09
Byron Sommardahl <byron@acklenavenue.com>
2018-09-14 20:46:06 -0500

    Tested several scenarios. Made jwt a global requirement.

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Get, Controller, UseGuards, Req } from '@nestjs/common';
+import { Req, Get, Controller } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
