31f39c979e067f771c52aca4c24b193ddbf85049
cefeboru <cefeboru@gmail.com>
2018-09-15 11:36:38 -0600

    Using JwtGuard at Controller level

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,11 @@
-import { Req, Get, Controller } from '@nestjs/common';
+import { Req, Get, Controller, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiBearerAuth } from '@nestjs/swagger'
+import { JwtAuthGuard } from './modules/auth/jwt-auth.guard';
 
 @Controller()
+@ApiBearerAuth()
+@UseGuards(JwtAuthGuard)
 export class AppController {
   constructor(private readonly appService: AppService) { }
 
